1871ba0b40bcb20e24f6d0afdc01d6b0a2e9d21e
smc8501 <s.marijuancarreno9378@gmail.com>
2026-07-06 18:53:27 +0100

    Add reducers addItem, removeItem, updateQuantity to CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,11 +7,23 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;  // Increment quantity if item already exists
+      } else {
+        state.items.push({ name, image, cost, quantity:1});
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
+      const itemToUpdate = state.items.find(item => item.name === action.payload.name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity; // If the item is found, update its quantity to the new value
+      }
 
     
     },
