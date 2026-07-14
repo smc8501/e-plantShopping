0ea389dd30fdd2d9a086d51847954080b0dd217c
smc8501 <s.marijuancarreno9378@gmail.com>
2026-07-14 13:21:40 +0100

    Add handleAddToCart functionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useDispatch } from 'react-redux';
+import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
+    const dispatch = useDispatch();
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({}); // State to keep track of added plants
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -252,6 +255,18 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [product.name]: true, 
+            // Mark the product as added to cart
+        }));
+        alert(`${product.name} has been added to the cart!`);
+
+    }
     return (
         <div>
             <div className="navbar" style={styleObj}>
